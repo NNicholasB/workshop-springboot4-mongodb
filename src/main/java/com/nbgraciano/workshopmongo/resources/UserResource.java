107667ee08ff3e09335ad9ba43175ea78b2a3c11
Nicholas Bernardo <nicholasbgraciano@gmail.com>
2026-06-08 19:52:17 -0300

    created endpoint for users posts

--- src/main/java/com/nbgraciano/workshopmongo/resources/UserResource.java
+++ src/main/java/com/nbgraciano/workshopmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.nbgraciano.workshopmongo.resources;
 
+import com.nbgraciano.workshopmongo.domain.Post;
 import com.nbgraciano.workshopmongo.domain.User;
 import com.nbgraciano.workshopmongo.dto.UserDTO;
 import com.nbgraciano.workshopmongo.services.UserService;
@@ -54,5 +55,11 @@ public class UserResource {
         obj.setId(id);
         obj=service.update(obj);
         return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(value="/{id}/posts",method= RequestMethod.GET)
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+        User obj=service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 }
